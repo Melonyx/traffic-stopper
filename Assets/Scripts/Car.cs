84751eedb9bcd62723e72bae343a88da29794e81
George Loginov <egorloginov857@gmail.com>
2019-10-13 17:40:49 +0400

    Add duration after car stop

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine.EventSystems;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Car : Unit, IPointerClickHandler
 {
+    private float _lastSpeed;
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        Speed = Speed == 0f ? Speed : 0f;
+        if (Speed != 0f)
+            StartCoroutine(StopWaitGo());
+    }
+
+    private IEnumerator StopWaitGo()
+    {
+        _lastSpeed = Speed;
+        Speed = 0f;
+
+        yield return new WaitForSeconds(3.5f);
+
+        Speed = _lastSpeed;
     }
 }
